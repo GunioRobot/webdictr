0ca79933de8c5ff4bbf937e7d0934a411f240e08
Nguyen Duy Tho <nguyenduytho@gmail.com>
2008-12-26 18:13:55 +0700

    Add css and format output

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,9 +1,11 @@
 module WordsHelper
   def format_definition(text)
-    result = ""
+    result = "<ul>"
     text.each_line do |line|
-      result << "<p> #{line}</p>"
-    end 
-    result
+      result << line.sub(/^\*(.+)/,'<li class="wordtype">\1</li>') if line.match /^\*.*/
+      result << line.sub(/^\-(.+)/,'<li class="meaning">\1</li>') if line.match /^\-.*/
+      result << line.sub(/^\=(.+)\+(.*)/,'<li class="example">\1</li> <li class="example-meaning">\2</li>') if line.match /^\=.*/
+    end
+    result << "</ul>"
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,10 @@
 class Word < ActiveRecord::Base
   belongs_to :dict
-  
+  validates_presence_of :keyword
+  validates_presence_of :definition
+
   define_index do
-    indexes keyword, :sortable => true  
+    indexes keyword, :sortable => true
     indexes definition
   end
 
